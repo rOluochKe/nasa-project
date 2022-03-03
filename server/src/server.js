cd9e1b3adc05ff26c8ef255e40e2e47f698ac1b1
roluochKe <rasholuoch@gmail.com>
2022-03-03 16:55:56 +0300

    persisting data with mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
 
@@ -6,10 +7,27 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 // const PORT = process.env.PORT || 8000
 const PORT = 5000
+const MONGO_URL =
+  'mongodb+srv://nasausr:nasaDB22@cluster0.66lfq.mongodb.net/nasaDbApi?retryWrites=true&w=majority'
 
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDb connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+
   await loadPlanetsData()
 
   server.listen(PORT, () => {
